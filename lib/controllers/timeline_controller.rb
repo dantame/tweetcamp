bf276736ea84cf2c7adc972efc77b87efa8bc03d
Tame, Dan <Dan.Tame@sky.uk>
2015-07-14 15:02:51 +0100

    cleanup code

--- lib/controllers/timeline_controller.rb
+++ lib/controllers/timeline_controller.rb
@@ -2,22 +2,13 @@ require 'sinatra/base'
 
 module TweetCamp
   class TimelineController < Sinatra::Base
-
-    def get_tweets username = nil
-      if username
-        settings.twitter.user_timeline username
-      else
-        settings.twitter.user_timeline
-      end
-    end
-
     get '/?:username?' do
       username = params[:username]
       data = nil
       if username
-        data = get_tweets username
+        data = settings.twitter.user_timeline username
       elsif settings.twitter.credentials?
-        data = get_tweets
+        data = settings.twitter.user_timeline
       end
       if data
         erb :index, :locals => {twitter: settings.twitter, header_links: settings.links, tweets: data}
